814917fd5f714cf8f475af16d4bb82492a20344f
anti-fe <raylyangosha@gmail.com>
2022-02-28 11:03:17 +0300

    add about-us

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@ let swiper;
 swiper = new Swiper(slider, {
     spaceBetween: 30,
     centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
